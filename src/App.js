8f466f26abc878724adc8ebb0446dc29252fc4ab
Sakinah Hussain <hussainsakinah2611@gmail.com>
2024-01-28 20:35:45 +0530

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import WelcomeMessage from "./components/welcome";
+import StudentInfo from "./components/student-info";
 function App() {
-  return (
-    <div class="right-align">
-      <h1>Roll No - 2211CS010513 </h1>
-      <h1>Name - Sakinah Hussain </h1>
-      <h1>CGPA - 9.18 </h1>
-    </div>
-  );
+return (
+<div className="App">
+<div class="container-fluid">
+<WelcomeMessage/>
+<StudentInfo/>
+</div>
+</div>
+);
 }
-
 export default App;
---
+++ src/Components/student-info.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const StudentInfo = () => {
+return (
+<div>
+<table>
+<tbody>
+<tr>
+<td>Roll No.</td>
+<td>2211CS010513</td>
+</tr>
+<tr>
+<td>Name</td>
+<td>SAKINAH</td>
+</tr>
+</tbody>
+</table>
+</div>
+);
+};
+export default StudentInfo;
